a24249229d337d3594f9ab7aa23bf69c781dacab
chenzhihao <444745454@qq.com>
2019-06-14 10:10:04 +0800

    本地工程添加到commit

---
+++ test-git/src/com/czh/git/TestGit.java
@@ -0,0 +1,8 @@
+package com.czh.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("好好学习 天天向上");
+        System.out.println("争取明年买车见家长,给爸妈减轻压力");
+    }
+}
