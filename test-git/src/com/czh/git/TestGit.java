265993f9d9046abcf4840655a898eeaff020c049
chenzhihao <444745454@qq.com>
2019-06-14 10:25:01 +0800

    修改了testGit给自己加油

--- test-git/src/com/czh/git/TestGit.java
+++ test-git/src/com/czh/git/TestGit.java
@@ -4,10 +4,12 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("好好学习 天天向上");
         System.out.println("争取明年买车见家长,给爸妈减轻压力");
+        System.out.println("加油加油加油");
     }
 
     public void good(){
         int i = 1;
         System.out.println(i+1);
+        System.out.println("加油呀");
     }
 }
